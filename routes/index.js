19dab352ef495da6913285e019dc7d2b0232ef44
Guille <lissguillermo@gmail.com>
2014-07-02 16:12:18 +0000

    move stream to index page

--- routes/index.js
+++ routes/index.js
@@ -10,26 +10,7 @@ var api500px = new API500px(consumerKey);
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-/* 
-** Pictures stream with dynamic update
-*/
-router.get('/stream', function(req, res) {
-  res.render('stream', { title: 'Dynamic photo stream' });
-});
-
-/* 
-** Pictures stream with no update nor animation
-*/
-router.get('/staticstream', function(req, res) {
-	api500px.photos.getFreshToday({'sort': 'created_at', 'rpp': '20'},  function(error, results) {
-		if (error) {
-			return;
-		}
-		res.render('staticstream', { title: 'Static Photo Stream', data: results['photos'] });
-	});	
+  res.render('stream', { title: '500px fresh pictures' });
 });
 
 module.exports = router;
