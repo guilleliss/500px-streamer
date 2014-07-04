9efd2ccc1747128547b80a464f79f0cf0ee517d3
Guille <lissguillermo@gmail.com>
2014-07-04 19:49:51 +0000

    showing pics on map first version

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,12 @@ var http = require('https');
 var router = express.Router();
 var fs = require('fs');
 
-/* 500px API stuff */
-consumerKey = "67OCFqsvMd9r1DvdfPrOcrjJA17XptHNFsaTOPwM";
-var API500px = require('500px').API500px;
-var api500px = new API500px(consumerKey);
-
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('stream', { title: '500px fresh pictures' });
+});
+
+router.get('/map', function(req, res) {
+  res.render('map', { title: '500px fresh pictures on the map' });
 });
 
 module.exports = router;
